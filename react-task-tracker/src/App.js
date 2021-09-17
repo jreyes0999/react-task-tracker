623b999feb7d619ebfc2055cddcc38c9c089b6c7
Defluxit <astroghost1@gmail.com>
2021-09-16 23:24:34 -0500

    Initial tracker

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import React from 'react'
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header />
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
+}
+
+export default App;
